ea54c347b9b7645011dc854542f27ec9baa3ba0c
ryanb <ryanbradford333@gmail.com>
2016-09-18 16:31:32 -0400

    Prepared for release.

---
+++ src/com/ryanb3/ImageToPoly/Test/ImageToPoly.java
@@ -0,0 +1,13 @@
+package com.ryanb3.ImageToPoly.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+import com.ryanb3.ImageToPoly.Main;
+
+public class ImageToPoly {
+
+	public static void main(String[] args) throws InvocationTargetException, InterruptedException {
+		new Main();
+	}
+	
+}
